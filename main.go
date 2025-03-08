2b527a67fbd1161c2452a8842a58a252c3268375
Rpstvs <125133153+rpstvs@users.noreply.github.com>
2025-03-08 10:20:37 +0000

    adding reading 8 bytes

--- main.go
+++ main.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"log"
+	"os"
+)
 
 func main() {
-	fmt.Println("I hope I get the job!")
+	file, err := os.Open("messages.txt")
+
+	if err != nil {
+		log.Fatal("couldnt open file")
+	}
+
+	defer file.Close()
+
+	buffer := make([]byte, 8)
+
+	for {
+		_, err := file.Read(buffer)
+		fmt.Printf("read: %s \n", buffer)
+		if err == io.EOF {
+			break
+		}
+	}
 }
