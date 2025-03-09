e1065877602b1bc48106ec4c1d9f48955a043ff8
Rpstvs <125133153+rpstvs@users.noreply.github.com>
2025-03-09 12:02:28 +0000

    parsing the file by new lines

--- main.go
+++ main.go
@@ -1,28 +1,49 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"io"
 	"log"
 	"os"
+	"strings"
 )
 
+const inputFilePath = "messages.txt"
+
 func main() {
-	file, err := os.Open("messages.txt")
+	file, err := os.Open(inputFilePath)
 
 	if err != nil {
-		log.Fatal("couldnt open file")
+		log.Fatalf("couldnt open file %s: %s\n", inputFilePath, err)
 	}
 
-	defer file.Close()
-
-	buffer := make([]byte, 8)
-
+	fmt.Printf("Reading data from %s\n", inputFilePath)
+	fmt.Println("=====================================")
+	currentLine := ""
 	for {
-		_, err := file.Read(buffer)
-		fmt.Printf("read: %s \n", buffer)
-		if err == io.EOF {
+		buffer := make([]byte, 8, 8)
+		n, err := file.Read(buffer)
+
+		if err != nil {
+			if errors.Is(err, io.EOF) {
+				fmt.Printf("read: %s \n", currentLine)
+				break
+			}
+			fmt.Printf("error: %s\n", err.Error())
 			break
 		}
+		str := string(buffer[:n])
+
+		parts := strings.Split(str, "\n")
+
+		if len(parts) > 1 {
+			currentLine += parts[0]
+			fmt.Printf("read: %s \n", currentLine)
+			currentLine = parts[1]
+		} else {
+			currentLine += parts[0]
+		}
+
 	}
 }
