03ead31c5397c9d6b6861f6eeb7841cc3eb7bba6
Rpstvs <125133153+rpstvs@users.noreply.github.com>
2025-03-27 17:58:50 +0000

    added new tests

--- internal/request/request_test.go
+++ internal/request/request_test.go
@@ -1,11 +1,13 @@
 package request
 
 import (
+	"strings"
 	"testing"
 
-	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestRequestLineParser(t *testing.T) {
-	assert.Equal(t, "TheTestagen", "TheTestagen")
+	r, err := RequestFromReader(strings.Reader("GET / HTTP/1.1\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n"))
+	require.NoError(t, err)
 }
