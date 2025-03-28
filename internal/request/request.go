7a2f953688572ea07b9e6ee02403bb24ae0e8b56
Rpstvs <125133153+rpstvs@users.noreply.github.com>
2025-03-28 09:57:13 +0000

    continuing with parser

--- internal/request/request.go
+++ internal/request/request.go
@@ -1,6 +1,11 @@
 package request
 
-import "io"
+import (
+	"fmt"
+	"io"
+	"strings"
+	"unicode"
+)
 
 type Request struct {
 	RequestLine RequestLine
@@ -14,4 +19,33 @@ type RequestLine struct {
 
 func RequestFromReader(reader io.Reader) (*Request, error) {
 
+	dat, err := io.ReadAll(reader)
+
+	if err != nil {
+		fmt.Println(err)
+		return &Request{}, nil
+	}
+
+	reqParts := strings.Split(string(dat), "\r\n")
+
+	reqLine := strings.Split(reqParts[0], " ")
+
+	if len(reqLine) != 3 {
+		return &Request{}, fmt.Errorf("bad request line")
+	}
+
+	if !isUpper(reqLine[0]) {
+		return &Request{}, fmt.Errorf()
+	}
+
+	return &Request{}, nil
+}
+
+func isUpper(s string) bool {
+	for _, r := range s {
+		if !unicode.IsUpper(r) && unicode.IsLetter(r) {
+			return false
+		}
+	}
+	return true
 }
--- internal/request/request_test.go
+++ internal/request/request_test.go
@@ -4,10 +4,27 @@ import (
 	"strings"
 	"testing"
 
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
 func TestRequestLineParser(t *testing.T) {
-	r, err := RequestFromReader(strings.Reader("GET / HTTP/1.1\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n"))
+	r, err := RequestFromReader(strings.NewReader("GET / HTTP/1.1\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n"))
 	require.NoError(t, err)
+	require.NotNil(t, r)
+
+	assert.Equal(t, "GET", r.RequestLine.Method)
+	assert.Equal(t, "/", r.RequestLine.RequestTarget)
+	assert.Equal(t, "1.1", r.RequestLine.HttpVersion)
+
+	r, err = RequestFromReader(strings.NewReader("GET /coffee HTTP/1.1\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n"))
+	require.NoError(t, err)
+	require.NotNil(t, r)
+
+	assert.Equal(t, "GET", r.RequestLine.Method)
+	assert.Equal(t, "/coffee", r.RequestLine.RequestTarget)
+	assert.Equal(t, "1.1", r.RequestLine.HttpVersion)
+
+	_, err = RequestFromReader(strings.NewReader("GET /coffee HTTP/1.1\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n"))
+	require.Error(t, err)
 }
