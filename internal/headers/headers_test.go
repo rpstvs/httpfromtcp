57421e68cb9b87a463f43dfbed4805a05d8f0ef3
Rpstvs <125133153+rpstvs@users.noreply.github.com>
2025-04-04 13:43:55 +0100

    added new tests for headers

--- internal/headers/headers_test.go
+++ internal/headers/headers_test.go
@@ -8,6 +8,7 @@ import (
 )
 
 func TestHeaders(t *testing.T) {
+	//test valid single header
 	headers := NewHeaders()
 	data := []byte("Host: localhost:42069\r\n\r\n")
 
@@ -15,8 +16,53 @@ func TestHeaders(t *testing.T) {
 
 	require.NoError(t, err)
 	require.NotNil(t, headers)
-	assert.Equal(t, "localhost:42069", headers["Host"])
-	assert.Equal(t, 23, n)
+	assert.Equal(t, "localhost:42069", headers["host"])
+	assert.Equal(t, 25, n)
 	assert.False(t, done)
 
+	//test valid with extra whitespace
+	headers = NewHeaders()
+	data = []byte("       Host: localhost:42069                           \r\n\r\n")
+
+	n, done, err = headers.Parse(data)
+
+	require.NoError(t, err)
+	require.NotNil(t, headers)
+	assert.Equal(t, "localhost:42069", headers["host"])
+	assert.Equal(t, 57, n)
+	assert.False(t, done)
+
+	//test valid with extra whitespace
+	headers = map[string]string{"host": "localhost:42069"}
+	data = []byte("User-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n")
+
+	n, done, err = headers.Parse(data)
+
+	require.NoError(t, err)
+	require.NotNil(t, headers)
+	assert.Equal(t, "localhost:42069", headers["host"])
+	assert.Equal(t, "curl/7.81.0", headers["user-agent"])
+	assert.Equal(t, 25, n)
+	assert.False(t, done)
+
+	//test valid done
+
+	headers = NewHeaders()
+	data = []byte("\r\n a bunch of other stuff")
+
+	n, done, err = headers.Parse(data)
+
+	require.NoError(t, err)
+	require.NotNil(t, headers)
+	assert.Empty(t, headers)
+	assert.Equal(t, 2, n)
+	assert.True(t, done)
+
+	// Test: Invalid character header
+	headers = NewHeaders()
+	data = []byte("H©st: localhost:42069\r\n\r\n")
+	n, done, err = headers.Parse(data)
+	require.Error(t, err)
+	assert.Equal(t, 0, n)
+	assert.False(t, done)
 }
