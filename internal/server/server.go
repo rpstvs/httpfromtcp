06fe087a5c06bc9f871841fb6e008fb17469bd18
Rpstvs <125133153+rpstvs@users.noreply.github.com>
2025-04-11 11:37:53 +0100

    adding response

--- internal/server/server.go
+++ internal/server/server.go
@@ -1,12 +1,57 @@
 package server
 
+import (
+	"fmt"
+	"log"
+	"net"
+	"sync/atomic"
+)
+
 type Server struct {
+	listener net.Listener
+	closed   atomic.Bool
 }
 
 func Serve(port int) (*Server, error) {
-	return &Server{}, nil
+	listener, err := net.Listen("tcp", fmt.Sprint(":%d", port))
+	if err != nil {
+		return nil, err
+	}
+
+	server := Server{
+		listener: listener,
+	}
+
+	go server.listen()
+
+	return &server, nil
+}
+
+func (server *Server) listen() {
+	for {
+		conn, err := server.listener.Accept()
+		if err != nil {
+			log.Println("couldnt establish conn")
+		}
+		server.handle(conn)
+	}
+
 }
 
 func (server *Server) Close() {
+	server.listener.Close()
+}
 
+func (server *Server) handle(conn net.Conn) {
+	defer conn.Close()
+
+	response := "HTTP/1.1 200 OK\r\n" +
+		"Content-Type: text/plain\r\n" +
+		"\r\n" +
+		"Hello World!"
+
+	_, err := conn.Write([]byte(response))
+	if err != nil {
+		log.Println("couldnt write response")
+	}
 }
