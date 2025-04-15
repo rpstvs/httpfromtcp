befb9ef917fb432768bcbd151065e5b773d479ba
Rpstvs <125133153+rpstvs@users.noreply.github.com>
2025-04-15 19:29:43 +0100

    finished response

--- internal/response/headers.go
+++ internal/response/headers.go
@@ -7,31 +7,6 @@ import (
 	"github.com/rpstvs/httpfromtcp/internal/headers"
 )
 
-type StatusCode int
-
-const (
-	StatusOK                  = 200
-	StatusBadRequest          = 400
-	StatusInternalServerError = 500
-)
-
-var StatusLine = map[StatusCode]string{
-	200: "HTTP/1.1 200 OK",
-	400: "HTTP/1.1 400 Bad Request",
-	500: "HTTP/1.1 500 Internal Server Error",
-}
-
-func WriteStatusLine(w io.Writer, statusCode StatusCode) error {
-	val, ok := StatusLine[statusCode]
-
-	if !ok {
-		return fmt.Errorf("status code not found")
-	}
-
-	w.Write([]byte(val))
-	return nil
-}
-
 func GetDefaultHeaders(contentLen int) headers.Headers {
 	headersResp := headers.NewHeaders()
 	headersResp.Set("Content-Length", fmt.Sprintf("%d", contentLen))
@@ -43,7 +18,7 @@ func GetDefaultHeaders(contentLen int) headers.Headers {
 
 func WriteHeaders(w io.Writer, headers headers.Headers) error {
 	for k, v := range headers {
-		_, err := w.Write([]byte(fmt.Sprintf("%s:%s", k, v)))
+		_, err := w.Write([]byte(fmt.Sprintf("%s: %s\r\n", k, v)))
 		if err != nil {
 			return err
 		}
