e01e60a6599541e426814fcf3eebed0d84e72543
Rpstvs <125133153+rpstvs@users.noreply.github.com>
2025-04-21 18:23:48 +0100

    added writer for chunked body

--- internal/response/writer.go
+++ internal/response/writer.go
@@ -66,3 +66,43 @@ func (w *Writer) WriteBody(p []byte) (int, error) {
 
 	return w.writer.Write(p)
 }
+
+func (w *Writer) WriteChunkedBody(p []byte) (int, error) {
+	if w.writerState != writerStateBody {
+		return 0, fmt.Errorf("cannot write body in state %d", w.writerState)
+	}
+	chunksize := len(p)
+
+	nTotal := 0
+
+	n, err := fmt.Fprintf(w.writer, "%x\r\n", chunksize)
+
+	if err != nil {
+		return nTotal, err
+	}
+
+	nTotal += n
+
+	n, err = w.writer.Write([]byte("\r\n"))
+
+	if err != nil {
+		return nTotal, err
+	}
+
+	nTotal += n
+
+	return nTotal, err
+}
+
+func (w *Writer) WriteChunkedBodyDone() (int, error) {
+	if w.writerState != writerStateBody {
+		return 0, fmt.Errorf("cant write body on state %d", w.writerState)
+	}
+	n, err := w.writer.Write([]byte("0\r\n\r\n"))
+
+	if err != nil {
+		return n, err
+	}
+
+	return n, nil
+}
